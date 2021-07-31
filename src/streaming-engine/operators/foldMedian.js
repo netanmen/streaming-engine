e9d9cebc28bcbf50033c824d1b4fb8eba02da8cd
Netanel Mendel <netanel.mendel@storrsoft.com>
2021-07-31 14:12:33 +0300

    handle foldMedian and foldSum errors

--- src/streaming-engine/operators/foldMedian.js
+++ src/streaming-engine/operators/foldMedian.js
@@ -6,6 +6,16 @@ class FoldMedian extends Transform {
     }
 
     _transform(data, _, callback) {
+        if (!data || !Array.isArray(data)) {
+            callback(new Error('FoldMedian error: input must be of type array!'));
+            return;
+        }
+
+        if (!data.length) {
+            callback(new Error('FoldMedian error: input should not be an empty array'));
+            return;
+        }
+
         const sortedData = data.sort((a, b) => a - b);
         const isArrayLengthEven = sortedData.length % 2 === 0;
         const halfArrayLength = (sortedData.length - 1) / 2;
--- src/streaming-engine/operators/foldSum.js
+++ src/streaming-engine/operators/foldSum.js
@@ -6,6 +6,16 @@ class FoldSum extends Transform {
     }
 
     _transform(data, _, callback) {
+        if (!data || !Array.isArray(data)) {
+            callback(new Error('FoldSum error: input must be of type array!'));
+            return;
+        }
+
+        if (!data.length) {
+            callback(new Error('FoldSum error: input should not be an empty array'));
+            return;
+        }
+
         const sum = data.reduce((sum, item) => sum + item, 0);
 
         callback(null, sum);
