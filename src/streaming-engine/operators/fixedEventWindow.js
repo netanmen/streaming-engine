34671e18c6dd77f2f9ec2eeb6b4f45a312b27404
Netanel Mendel <netanel.mendel@storrsoft.com>
2021-07-31 14:08:30 +0300

    handle FixedEventWindow errors

--- src/streaming-engine/operators/fixedEventWindow.js
+++ src/streaming-engine/operators/fixedEventWindow.js
@@ -8,6 +8,26 @@ class FixedEventWindow extends Transform {
     }
 
     _transform(data, _, callback) {
+        if (!data || !Number.isInteger(+data)) {
+            callback(new Error('FixedEventWindow error: input must be a number!'));
+            return;
+        }
+
+        if (this.size == 'undefind') {
+            callback(new Error('FixedEventWindow error: size is required'));
+            return;
+        }
+
+        if (!Number.isInteger(this.size)) {
+            callback(new Error('FixedEventWindow error: size must be of type number'));
+            return;
+        }
+
+        if (this.size <= 0) {
+            callback(new Error('FixedEventWindow error: size must be a positive number'));
+            return;
+        }
+
         this.fixedSizeArray.push(+data);
 
         if (this.fixedSizeArray.length === this.size) {
